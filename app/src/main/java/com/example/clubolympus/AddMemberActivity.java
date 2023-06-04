f0d7a3243412a21ee6926a0553d58c2cbe87f023
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-04 15:06:11 +0300

    add onItemSelectedListener for genderSpinner; add menu

--- app/src/main/java/com/example/clubolympus/AddMemberActivity.java
+++ app/src/main/java/com/example/clubolympus/AddMemberActivity.java
@@ -1,8 +1,13 @@
 package com.example.clubolympus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -35,5 +40,49 @@ public class AddMemberActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(spinnerAdapter);
+
+        //Присваиваем значение в gender по выбору в Spinner
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedGender = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    if (selectedGender.equals("Male")) {
+                        gender = 1;
+                    } else if (selectedGender.equals("Female")) {
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                gender = 0;
+            }
+        });
+    }
+
+    //Метод для отображения меню по созданной разметке из res/menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    //Метод для действия по нажатию кнопки в меню
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
