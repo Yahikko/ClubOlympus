2c4d55f4bba1babbd96958589b833d3db55a834f
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-06 22:57:52 +0300

    added CursorLoader

--- app/src/main/java/com/example/clubolympus/MainActivity.java
+++ app/src/main/java/com/example/clubolympus/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.clubolympus;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.clubolympus.data.MemberCursorAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.example.clubolympus.data.ClubOlympusContract.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private ListView lvMembers;
+    private static final int MEMBER_LOADER = 123;
+    MemberCursorAdapter memberCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +32,46 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton floatingActionButton =
                 findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddMemberActivity.class);
-                startActivity(intent);
-            }
+        floatingActionButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddMemberActivity.class);
+            startActivity(intent);
         });
-    }
 
-    private void displayData() {
-
-        String[] projection = {
-                MemberEntry._ID,
-                MemberEntry.COLUMN_FIRST_NAME,
-                MemberEntry.COLUMN_LAST_NAME,
-                MemberEntry.COLUMN_GENDER,
-                MemberEntry.COLUMN_SPORT};
-
-        Cursor cursor = getContentResolver().query(
-                MemberEntry.CONTENT_URI,
-                projection,
-                null,
-                null,
-                null);
-
-        MemberCursorAdapter memberCursorAdapter = new MemberCursorAdapter(this, cursor);
+        memberCursorAdapter = new MemberCursorAdapter(this, null);
         lvMembers.setAdapter(memberCursorAdapter);
-
+        LoaderManager.getInstance(this).initLoader(MEMBER_LOADER, null, this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        displayData();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+
+        String[] projection = {
+                MemberEntry._ID,
+                MemberEntry.COLUMN_FIRST_NAME,
+                MemberEntry.COLUMN_LAST_NAME,
+                MemberEntry.COLUMN_SPORT};
+
+        CursorLoader cursorLoader = new CursorLoader(this, MemberEntry.CONTENT_URI,
+                projection, null, null, null);
+
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+        memberCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+        memberCursorAdapter.swapCursor(null);
     }
 }
